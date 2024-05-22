f6b53102036a51ce03a5bb2af0fc83e79d34e406
zrafal <161606153+zrafal@users.noreply.github.com>
2024-05-22 12:53:43 +0000

    4_6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);  
     Service s = new Service();
 
     boolean exit = false;
